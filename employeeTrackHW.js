93f3c66b1294c1c1b18d268e7492100611a49210
StevenM404 <67655910+StevenM404@users.noreply.github.com>
2020-09-21 02:07:09 -0400

    added a lot of functions

--- employeeTrackHW.js
+++ employeeTrackHW.js
@@ -21,16 +21,127 @@ function trackerChoices(){
     inquirer.prompt({
         type: "list",
         message: "Choices",
+        name: "choices",
         choices: [
             "View All Employees",
-            "View All Employees By Roles",
-            "View All Employees By Departments",
+            "View All Roles",
+            "View All Departments",
             "Add Employee",
             "Add Department",
             "Add Role",
-            "Update Employee"
+            "Update Employee Role"
         ]
     }).then((answer) => {
-        
-    })
-}
+        switch (answer.choices) {
+            case "View All Employees":
+                viewAllEmployees();
+                break;
+
+            case "View All Roles":
+                viewAllRoles();
+                break;
+
+            case "View All Departments":
+                viewAllDepartments();
+                break;
+
+            case "Add Employee":
+                addEmployee();
+                break;
+
+            case "Add Department":
+                addDeptartment();
+                break;
+
+            case "Add Role":
+                addRole();
+                break;
+
+            case "Update Employee Role":
+                updateEmployeeRole();
+                break;
+        };
+    });
+};
+
+function viewAllEmployees() {
+    connection.query("SELECT * FROM employee", function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        trackerChoices();
+    });
+};
+
+function viewAllRoles() {
+    connection.query("SELECT * FROM role", function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        trackerChoices();
+    });
+};
+
+function viewAllDepartments() {
+    connection.query("SELECT * FROM department", function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        trackerChoices();
+    });
+};
+
+function addEmployee() {
+    inquirer.prompt([{
+        name: "firstName",
+        type: "input",
+        message: "First Name: "
+    },{
+        name: "lastName",
+        type: "input",
+        message: "Last Name: "
+    }]).then((answer) => {
+        connection.query("INSERT INTO employee SET ?", {
+            first_name: `${answer.firstName}`,
+            last_name: `${answer.lastName}`
+        },function (err, res) {
+            if (err) throw err;
+            console.log("Employee Added!");
+            trackerChoices();
+        });
+    });
+};
+
+function addDeptartment() {
+    inquirer.prompt([{
+        name: "name",
+        type: "input",
+        message: "Department Name: "
+    }]).then((answer) => {
+        connection.query("INSERT INTO department SET ?", {
+            name: `${answer.name}`
+        },function (err, res) {
+            if (err) throw err;
+            console.log("Department Added!");
+            trackerChoices();
+        });
+    });
+};
+
+function addRole() {
+    inquirer.prompt([{
+        name: "title",
+        type: "input",
+        message: "Title: "
+    },{
+        name: "salary",
+        type: "input",
+        message: "Salary: "
+    }]).then((answer) => {
+        connection.query("INSERT INTO role SET ?", {
+            title: `${answer.title}`,
+            salary: `${answer.salary}`
+        },function (err, res) {
+            if (err) throw err;
+            console.log("Role Added!");
+            trackerChoices();
+        });
+    });
+};
